60feda7c46062ebbac87b49a756903065a5ee407
Kevin Secretan <kevinmccloud@gmail.com>
2012-05-08 19:27:02 -0700

    more repl testing, ignore

--- hbasey/testddc.clj
+++ hbasey/testddc.clj
@@ -17,50 +17,6 @@
 
 )
 
-(def raw-data ^{:dimensions [:airline [:year :qtr] :origin] :measure :qty}
-  [[:alaska [2010 :q1] :ca] 2
-   [:alaska [2010 :q1] :wa] 2
-   [:alaska [2010 :q2] :ca] 5
-   [:alaska [2010 :q2] :or] 4
-   [:alaska [2010 :q2] :wa] 3
-   [:alaska [2010 :q3] :ca] 2
-   [:alaska [2010 :q4] :or] 2
-   [:alaska [2011 :q1] :wa] 4
-   [:alaska [2011 :q2] :ca] 2
-   [:delta [2010 :q1] :ca] 1
-   [:delta [2010 :q2] :wa] 3
-   [:delta [2010 :q3] :ca] 4
-   [:delta [2010 :q4] :or] 2
-   [:delta [2011 :q1] :wa] 4
-   [:united [2010 :q4] :or] 2
-   [:united [2011 :q1] :ca] 1
-   [:united [2011 :q1] :or] 2
-   [:united [2011 :q1] :wa] 4])
-
-(defn gen-ddc
-  "Generates a ddc with op (default sum) from static data. The op must fulfill the constraint
-  (a+b)-b=a"
-  ([data] (gen-ddc data +))
-  ([data op]
-   ))
-
-(defn query-ddc
-  [query & args]
-  )
-
-(defn query-cube
-  [cube query]
-  (let [[k v] (first (filter #(= query (first %1)) (partition 2 cube)))]
-    v))
-
-(query-raw-data [:delta [2011 :q1] :wa])
-
-(defn query2 [query]
-  (let [coords (take-nth 2 raw-data)
-        values (take-nth 2 (drop 1 raw-data))]
-    (nth values (.indexOf coords query))))
-(query2 [:delta [2011 :q1] :wa])
-
 (def orig ^{:dimensions [:d1 :d2] :measure :val :N 10}
  [[0 0] 3 [0 1] 5 [0 2] 1 [0 3] 2 [0 4] 2 [0 5] 4 [0 6] 6 [0 7] 3 [0 8] 3 [0 9] 1 [1 0] 7 [1 1] 3 [1 2] 2 [1 3] 6 [1 4] 8 [1 5] 7 [1 6] 1 [1 7] 2 [1 8] 4 [1 9] 2 [2 0] 2 [2 1] 4 [2 2] 2 [2 3] 3 [2 4] 3 [2 5] 3 [2 6] 4 [2 7] 5 [2 8] 7 [2 9] 4 [3 0] 3 [3 1] 2 [3 2] 1 [3 3] 5 [3 4] 3 [3 5] 5 [3 6] 2 [3 7] 8 [3 8] 2 [3 9] 1 [4 0] 4 [4 1] 2 [4 2] 1 [4 3] 3 [4 4] 3 [4 5] 4 [4 6] 7 [0 0] 3 [0 1] 5 [0 2] 1 [0 3] 2 [0 4] 2 [0 5] 4 [0 6] 6 [0 7] 3 [0 8] 3 [0 9] 1 [1 0] 7 [1 1] 3 [1 2] 2 [1 3] 6 [1 4] 8 [1 5] 7 [1 6] 1 [1 7] 2 [1 8] 4 [1 9] 2 [2 0] 2 [2 1] 4 [2 2] 2 [2 3] 3 [2 4] 3 [2 5] 3 [2 6] 4 [2 7] 5 [2 8] 7 [2 9] 4 [3 0] 3 [3 1] 2 [3 2] 1 [3 3] 5 [3 4] 3 [3 5] 5 [3 6] 2 [3 7] 8 [3 8] 2 [3 9] 1 [4 0] 4 [4 1] 2 [4 2] 1 [4 3] 3 [4 4] 3 [4 5] 4 [4 6] 7 [4 7] 1 [4 8] 3 [4 9] 2 [5 0] 2 [5 1] 3 [5 2] 3 [5 3] 6 [5 4] 1 [5 5] 8 [5 6] 5 [5 7] 1 [5 8] 1 [5 9] 2 [6 0] 4 [6 1] 5 [6 2] 2 [6 3] 7 [6 4] 1 [6 5] 9 [6 6] 3 [6 7] 3 [6 8] 4 [6 9] 1 [7 0] 2 [7 1] 4 [7 2] 2 [7 3] 2 [7 4] 3 [7 5] 1 [7 6] 9 [7 7] 1 [7 8] 3 [7 9] 3 [8 0] 5 [8 1] 4 [8 2] 3 [8 3] 1 [8 4] 3 [8 5] 2 [8 6] 1 [8 7] 9 [8 8] 6 [8 9] 5 [9 0] 6 [9 1] 1 [9 2] 2 [9 3] 4 [9 4] 2 [9 5] 1 [9 6] 3 [9 7] 1 [9 8] 5 [9 9] 2])
 
@@ -71,39 +27,12 @@
    [2 5] 50 [2 6] 4 [2 7] 5 [2 8] 7 [2 9] 4 [3 0] 12 [3 1] 9 [3 2] 5 [3 3] 28
    [3 4] 14 [3 5] 69 [3 6] 7 [3 7] 15 [3 8] 35 [3 9] 7 [4 0] 16 [4 1] 2
    [4 2] 1 [4 3] 6 [4 4] 3 [4 5] 86 [4 6] 7 [4 7] 1 [4 8] 11 [4 9] 2
-   [5 0] 18 [5 1] 19 [5 2] 29 [5 3] 54 [5 4] 74 [5 5] 126 [5 6] 25 [5 7] 45
+   [5 0] 21 [5 1] 19 [5 2] 29 [5 3] 54 [5 4] 74 [5 5] 126 [5 6] 25 [5 7] 45
    [5 8] 65 [5 9] 77 [6 0] 4 [6 1] 5 [6 2] 2 [6 3] 14 [6 4] 1 [6 5] 28
    [6 6] 3 [6 7] 3 [6 8] 10 [6 9] 1 [7 0] 6 [7 1] 4 [7 2] 2 [7 3] 8 [7 4] 3
    [7 5] 42 [7 6] 9 [7 7] 1 [7 8] 13 [7 9] 3 [8 0] 11 [8 1] 13 [8 2] 7
    [8 3] 30 [8 4] 7 [8 5] 60 [8 6] 13 [8 7] 9 [8 8] 39 [8 9] 9 [9 0] 17
    [9 1] 1 [9 2] 2 [9 3] 7 [9 4] 2 [9 5] 76 [9 6] 3 [9 7] 1 [9 8] 9 [9 9] 2])
-
-; (map-indexed (fn [idx item] (map-indexed (fn [idx2 n] [[idx idx2] n]) item)) (map vec (partition 10 [paaste])))
-
-(comment "
-         Compute the top-most overlay. There will be 2^d equi-size boxen.
-         For the first box, (0,...,0):
-         ")
-
-
-(defn gen-ddc [orig]
-  (let [md (meta orig)
-        dims (range (count (md :dimensions)))
-        coords (take-nth 2 orig)
-        values (take-nth 2 (drop 1 orig))
-        N (count coords)
-        k (/ N 2)
-        ]))
-
-
-(= (gen-ddc orig) expected)
-
-(gen-ddc orig)
-
-;If it doesn't fit, grow the cube (n+1)*2 in each dimension.
-;Thus, if we start with 2x2, we grow to 6x6, then to 14x14, then to 30x30.
-;If we start with 4x4, we grow to 10x10, then to 22x22, and 46x46.
-;If we start with 1x1, we grow to 4x4. This is the algorithm, start with 1x1.
 
 (defn sort-cube [cube]
   (sort
@@ -133,17 +62,40 @@
 (<all 1 [2 3 4])
 (<all 1 [2 0 4])
 
-(defn insert-cube
+;summing
+(def op +)
+(def arc-op -)
+
+(defn insert-into-cube
   "Build cube iteratively, returns new cube."
-  [cube [cell value]]
-  (if (<all ((meta cube) :N) cell) ; cell within cube dimensions
-    (insert-ddc cube ((meta cube) :N) [cell value] [0 0]) ; insert it with respec to origin
-    ;else grow to (N+1)*2
-    ;(naively assuming we only have to grow once to fit)
-    (update-ddc (* 2 (inc ((meta cube) :N)))
-                cube
-                [cell value])
-  ))
+  [cube origin [cell value]]
+  (println (meta cube))
+  (let [N ((meta cube) :N)
+        location (where-is-cell cell origin N)]
+    (cond
+      (= location :outside)
+        (let [newcube (with-meta cube (assoc (meta cube) :N
+                                         (loop [loc location n N]
+                                           (if-let [newn (and
+                                                           (= location :outside)
+                                                           (* 2 (inc n)))]
+                                             (recur (where-is-cell cell
+                                                                   origin new-n)
+                                                    new-n)
+                                             n))))]
+          ; we "grew" cube to fit the data. We have to insert&fill in the border
+          ; cells and anchor cell(s) to their appropriate values
+          )
+      (= location :border) ; only need to update border cells that
+        ; include this one and exist after in any dimension until anchor is hit.
+        ; that is, 
+      (= location :anchor) ; anchors are the sum from origin to anchor
+        ; of original data, but cube is ddc. anchors at the origin are
+        ; themselves. Anchors not at the origin are computed by
+        ; value + .
+      
+      )))
+
 
 (defn insert-ddc [cube N [cell value] origin]
   ; assert that cell is within NxN
@@ -168,24 +120,19 @@
       ; is its own anchor
   ))
 
-(defn update-ddc
-  [N datacube [cell value]]
-  
-  )
-
 (defn manhattan-dist [v1 v2]
   (apply + (map #(Math/abs (- %1 %2)) v1 v2)))
 
 (defn anchor-slots [origin N] ; size 2^dim of origin
   (apply combin/cartesian-product (map #(range %1 (+ %1 N) (/ N 2)) origin)))
+(def anchor-slots (memoize anchor-slots))
 
 (defn gen-centers [anchors length]
   (map (fn [anchor] (map #(+ length %1) anchor)) anchors))
+(def gen-centers (memoize gen-centers))
 
-(defn where-is-cell [cell origin N]
-  ; if anchor cell, return anchor notice
-  ; if we need to descend, return notic of which box to descend to (2^d options)
-  ; if border cell, return border notice
+
+(defn where-is-cell "http://www.youtube.com/watch?v=JwRzi-E1l40" [cell origin N]
   (let [anchors (anchor-slots origin N)
         length (/ N 4.0)
         in-hypercube? (fn [center] ; returns the given center point of
@@ -198,17 +145,34 @@
                             center
                             ; else we're in a border-cell
                             false)))]
-    (if (not= -1 (.indexOf anchors cell))
-      :anchor
-      ; check quadrants in parallel, if nil, it's a border cell
-      (if-let [center-quad (some identity (pmap in-hypercube?
-                                                (gen-centers anchors length)))]
-        center-quad
-        :border))))
+    (if (some #(>= %1 N) cell) :outside
+      (if (not= -1 (.indexOf anchors cell))
+        :anchor
+        ; check quadrants in parallel, if nil, it's a border cell
+        (if-let [center-q (some identity (pmap in-hypercube?
+                                               (gen-centers anchors length)))]
+          center-q
+          :border)))))
+(def where-is-cell (memoize where-is-cell))
 
 ;(def testcube ^{:N 1 :dimensions [:x :y]} [[0 0] 3]) ; initial 1x1
 
-(def testcube ^{:N 0 :dimensions [:x :y]} [])
+(def testcube ^{:N 1 :dimensions [:x :y]} [])
+
+(insert-cube testcube [0 0] [[0 0] 3])
+
+(insert-cube testcube [0 0] [[0 1] 5])
+(insert-cube testcube [0 0] [[0 2] 1])
+(insert-cube testcube [0 0] [[0 3] 2])
+(insert-cube testcube [0 0] [[0 4] 2])
+
+(insert-cube testcube [0 0] [[0 5] 4])
+
+(insert-cube testcube [0 0] [[0 6] 6])
+(insert-cube testcube [0 0] [[0 7] 3])
+(insert-cube testcube [0 0] [[0 8] 3])
+
+(insert-cube testcube [0 0] [[0 9] 1])
 
 (def allbut00 (drop 1 (for [x (range 10) y (range 10)] [x y])))
 
@@ -233,3 +197,37 @@
     (read)
     (println (map #(do (println %1 (add2cube %1 cube)) *1) row))
     (recur (next rows) cube)))
+
+(def raw-data ^{:dimensions [:airline [:year :qtr] :origin] :measure :qty}
+  [[:alaska [2010 :q1] :ca] 2
+   [:alaska [2010 :q1] :wa] 2
+   [:alaska [2010 :q2] :ca] 5
+   [:alaska [2010 :q2] :or] 4
+   [:alaska [2010 :q2] :wa] 3
+   [:alaska [2010 :q3] :ca] 2
+   [:alaska [2010 :q4] :or] 2
+   [:alaska [2011 :q1] :wa] 4
+   [:alaska [2011 :q2] :ca] 2
+   [:delta [2010 :q1] :ca] 1
+   [:delta [2010 :q2] :wa] 3
+   [:delta [2010 :q3] :ca] 4
+   [:delta [2010 :q4] :or] 2
+   [:delta [2011 :q1] :wa] 4
+   [:united [2010 :q4] :or] 2
+   [:united [2011 :q1] :ca] 1
+   [:united [2011 :q1] :or] 2
+   [:united [2011 :q1] :wa] 4])
+
+(defn query-cube
+  [cube query]
+  (let [[k v] (first (filter #(= query (first %1)) (partition 2 cube)))]
+    v))
+
+(query-raw-data [:delta [2011 :q1] :wa])
+
+(defn query2 [query]
+  (let [coords (take-nth 2 raw-data)
+        values (take-nth 2 (drop 1 raw-data))]
+    (nth values (.indexOf coords query))))
+(query2 [:delta [2011 :q1] :wa])
+
